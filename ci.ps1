a8444f0ddd664473593a0eddc03cce7b4bb2f1a1
stijn <stijn@ignitron.net>
2022-05-05 17:03:39 +0200

    all: Add CI builds

---
+++ ci.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+Write-Host ($PSVersionTable | Out-String)
+Import-Module Pester
+$config = [PesterConfiguration]::Default
+$config.Run.Path = $PSScriptRoot
+$config.Run.PassThru = $True
+$config.TestResult.Enabled = $True
+$res = Invoke-Pester -Configuration $config
+if ($env:APPVEYOR_JOB_ID) {
+  [System.Net.WebClient]::new().UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+    (Resolve-Path $config.TestResult.OutputPath.Value))
+}
+if ($res.FailedCount -gt 0) {
+  throw "$($res.FailedCount) tests failed"
+}
